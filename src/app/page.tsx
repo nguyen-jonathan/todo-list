696dda1be6586c240b89c50658a7e78512c5a0ce
naniwin <njonathan990@gmail.com>
2023-08-29 23:33:19 -0500

    link to about page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
+import Link from 'next/link';
+
 export default function Home() {
-  return <h1>Hi</h1>;
+  return (
+    <>
+      <header>
+        <h1 className="text-2xl">Todos</h1>
+        <Link href="/about">About</Link>
+      </header>
+      <ul></ul>
+    </>
+  );
 }
