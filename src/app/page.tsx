71269d7e5f640ed63732f9f73a434e61d280ce12
naniwin <njonathan990@gmail.com>
2023-08-30 00:01:56 -0500

    test complete checkbox. changed about page to new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,29 @@
+import {TodoItem} from '@/components/TodoItems';
+import {prisma} from '@/db';
 import Link from 'next/link';
 
-export default function Home() {
+function getTodos() {
+  return prisma.todo.findMany();
+}
+
+export default async function Home() {
+  const todos = await prisma.todo.findMany();
+  // await prisma.todo.create({data: {title: 'test', complete: false}});
   return (
     <>
       <header className="flex justify-between items-center mb-4">
         <h1 className="text-2xl">Todos</h1>
         <Link
           className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
-          href="/about">
-          About
+          href="/new">
+          New
         </Link>
       </header>
-      <ul></ul>
+      <ul className="pl-4">
+        {todos.map((todo) => (
+          <TodoItem key={todo.id} {...todo} />
+        ))}
+      </ul>
     </>
   );
 }
