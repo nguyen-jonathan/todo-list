8c6bf8a028c8b10489f4526ea99aedb8d6abf9a4
naniwin <njonathan990@gmail.com>
2023-08-29 23:36:25 -0500

    button design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,13 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <>
-      <header>
+      <header className="flex justify-between items-center mb-4">
         <h1 className="text-2xl">Todos</h1>
-        <Link href="/about">About</Link>
+        <Link
+          className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+          href="/about">
+          About
+        </Link>
       </header>
       <ul></ul>
     </>
